1955d9c8c3c1722eb2110aa0f763fb7961d1bad6
songey <46361143+songey@users.noreply.github.com>
2019-06-19 17:33:17 -0700

    Bjs src files initializer/application (#3)
    * Adding final services folder
    
    * Adding final ServletInitializer and ProjectApplication .java files

---
+++ src/main/java/com/codingdojo/ninjaProject/NinjaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.ninjaProject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaProjectApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(NinjaProjectApplication.class, args);
+		
+	}
+}
